364dc4c0b51bb40a8c44a8c8e553c20fdd943f58
Shuvendu Dhal <36043010+shuvenduoffline@users.noreply.github.com>
2020-04-16 11:02:26 +0530

    Initial upload

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: './dist/bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.ts?$/,
+            loader: 'ts-loader'
+        }]
+    }
+}
---
+++ webpack.config.test.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: './test/index.ts',
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.ts?$/,
+            loader: 'ts-loader'
+        }]
+    }
+}
